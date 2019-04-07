a806103d51c81b72941c642ce10f499cbc6af5bd
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-04-06 20:36:53 -0400

    add v1

---
+++ precache-manifest.4e26dd4489f42a98bdecafe34efc05a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e49a17bff20ec1e96b30",
+    "url": "/static/js/main.e49a17bf.chunk.js"
+  },
+  {
+    "revision": "5d9de64b43c58a570c98",
+    "url": "/static/js/2.5d9de64b.chunk.js"
+  },
+  {
+    "revision": "e49a17bff20ec1e96b30",
+    "url": "/static/css/main.395731c6.chunk.css"
+  },
+  {
+    "revision": "5d9de64b43c58a570c98",
+    "url": "/static/css/2.a0024a20.chunk.css"
+  },
+  {
+    "revision": "4c318117e6850ccba1c1654f4a797e4b",
+    "url": "/index.html"
+  }
+];
